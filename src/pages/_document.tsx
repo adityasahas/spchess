e9728d8ebd955300c60f5c0dab725d04461b1531
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-09-15 18:05:54 -0700

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Chess Club website for Sierra Pacific High School Chess Club" />
+          <meta name="keywords" content="Chess Club, Sierra Pacific, Sierra Pacific High School," />
+          <meta name="author" content="adityasahas.tech" />
+
+          <meta property="og:title" content="SP Chess Club" />
+          <meta property="og:description" content="Chess Club website for Sierra Pacific High School Chess Club" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://spchess.org" />
+
+          <meta name="twitter:title" content="SP Chess Club" /> 
+          <meta name="twitter:description" content="Chess Club website for Sierra Pacific High School Chess Club" />
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
